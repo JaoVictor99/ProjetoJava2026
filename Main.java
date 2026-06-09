7fcd8c3eb8345683ef0739d80ec13d93d9d1df22
João Victor de Souza Silva <jooao.victor.souza99@gmail.com>
2026-06-09 19:54:09 -0300

    Teste Commit

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in);
     }
 }
---
+++ src/Controller/GerarRelatori.java
@@ -0,0 +1,11 @@
+package Controller;
+
+public interface GerarRelatori {
+
+    void RelatoriosFaltas();
+
+    void RelatorioNotas();
+
+    void RelatorioGeral();
+
+};
